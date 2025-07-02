95f769a32469f2ead796352e2520437d445c491c
Student0PHP <playivan56@gmail.com>
2025-07-02 13:41:46 +0300

    Add files via upload

---
+++ if,else,switch, ternary operator/if-elseif-else.php
@@ -0,0 +1,12 @@
+<?php
+
+$score = 85;
+if ($score >= 90) {
+    echo "Отлично!";
+} elseif ($score >= 70) {
+    echo "Хорошо!";
+} elseif ($score >= 50) {
+    echo "Удовлетворительно";
+} else {
+    echo "Не удовлетворительно";
+}
